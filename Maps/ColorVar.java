0780a0561077947ca056df4af4a1c9d51ae1d87a
Xpliot <80181145+Xpliot@users.noreply.github.com>
2021-08-02 23:44:57 -0400

    Add files via upload

---
+++ Maps/ColorVar.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+public class ColorVar {
+    public static Color red_Orange = new Color(255,69,0);
+    public static Color orange = new Color(255,131,0);
+    public static Color chili_Pepper = new Color(223,54,45);
+    public static Color chartreuse = new Color(183,172,68);
+    public static Color baby_Blue = new Color(196, 233, 236);
+    public static Color blue_Green = new Color(117,230,218);
+    public static Color Blue_Grotto = new Color(24,154,180);
+    public static Color Navy_Blue = new Color(5,68,94);
+}
